839cad6fbc169e4f7520964b40354ddb41eaef40
Samridhi <samridhimaheshwari@gmail.com>
2018-10-25 10:47:03 +0530

    changed chatbot slifghtly

--- chatbot.py
+++ chatbot.py
@@ -20,40 +20,51 @@ def text_to_vector(text):
      words = WORD.findall(text)
      return Counter(words)
 
-questions  = ["Hi", "Bye", " I Need help with investment", "Budget is 20000","Budget is 30000", "Budget is 50000", "What are my stock options?", "Max Stock Option?"]
-answers = ["Hello", "Cya", " Sure. What is your budget?", "Okay you have 3 options : S&P, Nifty50, Sensex", "Okay you have 3 options : 1. S&P, 2. Hongkong, 3. Sensex", "Okay you have 3 options : 1. S&P, 2. London, 3. Sensex","Your Options are : ", "Your Max option is:"]
+questions  = ["Hi", "Bye", " I want to invest", "Max Stock?", "Print prices of available stocks"]
+answers = ["Hello", "Cya", "Your Options are : ", "Your Max stock is:", "Okay here are the prices :"]
 stocks = {}
+price_stocks = {"S&P": 100, "Hongkong" : 200, "London" : 400, "Sensex" : 500, "Nifty50" : 200}
 kk = True
 while kk:
 	question = raw_input("H:").lower()
 	vector1 = text_to_vector(question)
 	response = "B:"
-	response_i = 0
+	response_i = -1
 	maxc=-1
+	cosine = -1
 	for i in range(len(questions)):
 		q = str(questions[i]).lower()
 		vector2 = text_to_vector(q)
 		cosine = get_cosine(vector1, vector2)
-		if maxc<=cosine:
+		if maxc<cosine and cosine!=0:
 			maxc = cosine 
-			response_i = i
+			response_i = i	
+	if(response_i == -1):
+		print("Sorry I do not understand")
+		exit()
 	print response + answers[response_i]
-	if "options" in answers[response_i]:
+	response = answers[response_i].lower()
+	if "options" in response:
+		print price_stocks
 		vector3 = text_to_vector(q)
 		qq = raw_input("Which option do you choose?(Write name of stock) : ")
 		un = raw_input("How many units of stock do you want to buy?")
 		print "You are buying " + str(qq) + " & units: " + str(un)
-		stocks[qq] = un	
-	if "Options" in answers[response_i]:
+		if qq in stocks:		
+			stocks[qq] = int(stocks[qq]) + int(un);
+		else:
+			stocks[qq] = un;	
+		print stocks
+	elif "max" in response:
+		maxs = 0	
+		maxd = " "	
 		for d in stocks:
-			print "Stock: " + d, "Shares: " + stocks[d]
-	if "Max" in answers[response_i]:
-			maxs = 0	
-			maxd = " "	
-			for d in stocks:
-				if(stocks[d] >maxs):
-					stocks[d] = maxs
-					maxd = d
-			print "Stock: " + str(maxd), "Shares: " +str(maxs)
-	if(answers[response_i] == "Cya"):
+			if(stocks[d] >maxs):
+				maxs = stocks[d]
+				maxd = d
+		print "Stock: " + str(maxd), " Shares: " +str(maxs) + " Total Price : " + str(int(maxs)*price_stocks[maxd])
+	elif "prices" in response:
+		for p in price_stocks:
+			print (p, price_stocks[p])
+	elif(answers[response_i] == "Cya"):
 		exit()	
